a35d703fec01ec52ee446672226df5b13e7b36a0
ayrton <ayrton.parkinson1@gmail.com>
2022-06-25 11:29:47 -0400

    Add #shortest_path to compute the shortest path from xx to yy given a chesspiece

--- game_board.rb
+++ game_board.rb
@@ -7,6 +7,27 @@ class GameBoard
     @board = Array.new(size) { Array.new(size) }
   end
 
+  def shortest_path(start, finish, piece = Knight)
+    return nil if out_of_bounds?(start) || out_of_bounds?(finish)
+
+    queue = [{ position: start, path: [start] }]
+    visited = []
+
+    until queue.empty?
+      current = queue.shift
+      next if visited.include?(current[:position])
+      return current[:path] if current[:position] == finish
+
+      visited << current[:position]
+      knight = piece.new(current[:position], self)
+      knight.moves.each do |move|
+        queue << { position: move, path: current[:path] + [move] }
+      end
+    end
+
+    nil
+  end
+
   # Takes array of coordinates [row, col] and returns bool if position in board limits
   def out_of_bounds?(coordinates)
     limit = board.size - 1
@@ -18,11 +39,5 @@ class GameBoard
 end
 
 board = GameBoard.new(8)
-puts board.out_of_bounds?([0, 0])
-puts board.out_of_bounds?([3, 3])
-puts board.out_of_bounds?([-1, 2])
-puts board.out_of_bounds?([-1, -2])
-puts board.out_of_bounds?([4, 4])
+p board.shortest_path([3,3], [0,0])
 
-knight = Knight.new([2, 2], board)
-p knight.moves
